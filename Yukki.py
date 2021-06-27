70bd2aed31418e8ae558076cb9b1aca048a67da5
okaybro526-1 <84863955+okaybro526-1@users.noreply.github.com>
2021-06-27 14:27:13 +0530

    Update Yukki.py

--- Yukki.py
+++ Yukki.py
@@ -13,7 +13,7 @@ from telethon.tl import functions
 from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from Utils import RAID, RRAID
-from login import events, NewMessage, event
+from login import NewMessage, events
 
 
 a = API_ID
