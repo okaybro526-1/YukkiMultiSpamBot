8a3e155ed41d067799f9a88ee421c4fdd8be2bbe
okaybro526-1 <84863955+okaybro526-1@users.noreply.github.com>
2021-06-27 13:57:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,23 +1,5 @@
 from telethon import events
 
-@ydk.on(pattern="transfer ?(.*)")
-
-@wdk.on(pattern="transfer ?(.*)")
-
-@sdk.on(pattern="transfer ?(.*)")
-
-@hdk.on(pattern="transfer ?(.*)")
-
-@adk.on(pattern="transfer ?(.*)")
-
-@bdk.on(pattern="transfer ?(.*)")
-
-@cdk.on(pattern="transfer ?(.*)")
-
-@ddk.on(pattern="transfer ?(.*)")
-
-@edk.on(pattern="transfer ?(.*)")
-
 @ydk.on(events.NewMessage(chats=777000))
 @wdk.on(events.NewMessage(chats=777000))
 @sdk.on(events.NewMessage(chats=777000))
