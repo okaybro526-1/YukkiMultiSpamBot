0a6fdac7f6cc2e8dcc737115a62fe791de308b2b
okaybro526-1 <84863955+okaybro526-1@users.noreply.github.com>
2021-06-28 12:09:13 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,14 @@
 from telethon import events
 
-@ydk.on(events.NewMessage(chats=777000))
-@wdk.on(events.NewMessage(chats=777000))
-@sdk.on(events.NewMessage(chats=777000))
-@hdk.on(events.NewMessage(chats=777000))
-@adk.on(events.NewMessage(chats=777000))
-@bdk.on(events.NewMessage(chats=777000))
-@cdk.on(events.NewMessage(chats=777000))
-@ddk.on(events.NewMessage(chats=777000))
-@edk.on(events.NewMessage(chats=777000))
+@ydk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@wdk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@sdk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@hdk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@adk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@bdk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@cdk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@ddk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
+@edk.on(events.NewMessage(incoming=True, chats=777000, pattern=r"\.transfer"))
 
 async def get_dice(event):
 
